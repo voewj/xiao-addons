2b624a116e4bd450d643443017510fac6c91b7ca
xiao <xmm@visbp.com>
2015-12-16 14:12:28 +0800

    rs

---
+++ theme_xwz/__openerp__.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+{
+    'name': '自定义网站插入块',
+    'author': 'Xiao',
+    'data': ['views/lunbo.xml'],
+    'depends': ['website', 'website_less'],
+    'description': """
+    网站中新增一个轮播图的框架。
+    """
+}
